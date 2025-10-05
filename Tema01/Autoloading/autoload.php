b94a82534904e66ff4719c884b40a100367ffa21
MCarmen <carmengarcianavarro19@gmail.com>
2025-10-05 17:41:34 +0200

    autoload02

--- Tema01/Autoloading/autoload.php
+++ Tema01/Autoloading/autoload.php
@@ -2,7 +2,7 @@
 spl_autoload_register(function($className){
         echo $className."\n";
         
-        $rutaBuena=str_replace("\\","/",$className);
+        $rutaBuena=str_replace("\\",DIRECTORY_SEPARATOR,$className);
 
         $filePath = __DIR__ . "/". $rutaBuena . ".php";
 
