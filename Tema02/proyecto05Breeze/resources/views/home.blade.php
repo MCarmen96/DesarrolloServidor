d44b0faaa59ef1755e4fd1cf89488ed365c31f00
Mcarmen <carmengarcianavarro19@gmail.com>
2026-01-08 17:32:54 +0100

    proyecto05 breeze

---
+++ Tema02/proyecto05Breeze/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+@section('content')
+
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary">Left</button>
+        <button type="button" class="btn btn-primary">Middle</button>
+        <button type="button" class="btn btn-primary">Right</button>
+    </div>
+
+@endsection
---
+++ Tema02/proyecto05Breeze/resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+
+<footer>
+    <h1>este es el footer</h1>
+</footer>
